a5b5f8f16fed0c2ab9957c4ba967537e741fc821
npham22 <npham22@horizon.csueastbay.edu>
2022-03-22 11:32:13 -0700

    Revamped GUI layout and added ability to edit entries

--- src/address/AddressBookApplicationGUI.java
+++ src/address/AddressBookApplicationGUI.java
@@ -1,10 +1,17 @@
 package address;
 
 import address.data.AddressBook;
+import address.data.AddressEntry;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddressBookApplicationGUI {
 
@@ -17,12 +24,35 @@ public class AddressBookApplicationGUI {
     private JTextArea displayArea;
     private JButton addButton;
     private JButton removeButton;
+    private JTextField firstNameField;
+    private JTextField lastNameField;
+    private JTextField streetField;
+    private JTextField cityField;
+    private JTextField stateField;
+    private JTextField zipField;
+    private JTextField phoneField;
+    private JTextField emailField;
+    private JList entryList;
+    private JButton editOKBtn;
+    private JButton cancelBtn;
+    private JLabel confirmChange;
+
+    List<AddressEntry> bookEntries = new ArrayList<AddressEntry>();
+    DefaultListModel listModel = new DefaultListModel();
 
     public AddressBookApplicationGUI() {
         displayButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                displayArea.setText(ab.list());
+                //displayArea.setText(ab.list());
+                listModel.clear();
+                bookEntries = ab.find("");
+
+                for (AddressEntry entry : bookEntries) {
+                    listModel.addElement(entry.getLastName() + ", " + entry.getFirstName());
+                }
+
+                entryList.setModel(listModel);
             }
         });
         addButton.addActionListener(new ActionListener() {
@@ -37,6 +67,56 @@ public class AddressBookApplicationGUI {
             public void actionPerformed(ActionEvent e) {
                 removeDialog.setTitle("Removing an AddressEntry");
                 removeDialog.setVisible(true);
+            }
+        });
+
+        entryList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent listSelectionEvent) {
+                if (!listSelectionEvent.getValueIsAdjusting()) {
+                    if (entryList.getSelectedIndex() != -1) {
+                        AddressEntry entry = bookEntries.get(entryList.getSelectedIndex());
+
+                        firstNameField.setText(entry.getFirstName());
+                        lastNameField.setText(entry.getLastName());
+                        streetField.setText(entry.getStreet());
+                        cityField.setText(entry.getCity());
+                        stateField.setText(entry.getState());
+                        zipField.setText(Integer.toString(entry.getZip()));
+                        phoneField.setText(entry.getPhone());
+                        emailField.setText(entry.getEmail());
+                    }
+                }
+            }
+        });
+
+        editOKBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String inputText;
+
+                if (entryList.getSelectedIndex() != -1) {
+                    AddressEntry entry = bookEntries.get(entryList.getSelectedIndex());
+
+                    if (!firstNameField.getText().equals(entry.getFirstName())) {
+                        entry.setFirstName(firstNameField.getText()); // Somewhat funky...
+                    }
+
+                    confirmChange.setVisible(false);
+                    editOKBtn.setVisible(false);
+                    cancelBtn.setVisible(false);
+                }
+            }
+        });
+
+        firstNameField.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                super.focusGained(e);
+
+                confirmChange.setVisible(true);
+                editOKBtn.setVisible(true);
+                cancelBtn.setVisible(true);
             }
         });
     }
